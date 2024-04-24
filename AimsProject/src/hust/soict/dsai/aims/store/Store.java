2a85c9fb1beb68b29135cec5c2a14b7ec9362c46
unknown <hoa2004t@gmail.com>
2024-04-24 16:36:10 +0700

    Update Store class

--- AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -1,21 +1,22 @@
 package src.hust.soict.dsai.aims.store;
 
 import src.hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import src.hust.soict.dsai.aims.media.Media;
 
 import java.util.ArrayList;
 
 public class Store {
-    public ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
-    public void addDVD (DigitalVideoDisc... discs) {
-        for (DigitalVideoDisc disc : discs) {
-            this.itemsInStore.add(disc);
+    public ArrayList<Media> itemsInStore = new ArrayList<Media>();
+    public void addMedia (Media... items) {
+        for (Media item : items) {
+            this.itemsInStore.add(item);
             System.out.println("The disc has been added to the store.");
         }
 
     }
-    public void removeDVD(DigitalVideoDisc... discs) {
-        for (DigitalVideoDisc disc : discs) {
-            this.itemsInStore.remove(disc);
+    public void removeMedia(Media... items) {
+        for (Media item : items) {
+            this.itemsInStore.remove(item);
             System.out.println("The disc has been removed out of the store.");
         }
     }
