2c0d3708a2fa8d9fe493981f3ab4b3cee4c15ffc
unknown <hoa2004st.com>
2024-05-26 21:54:29 +0700

    Preview

--- GUIProject/src/hust/soict/dsai/javafx/Main.java
+++ GUIProject/src/hust/soict/dsai/javafx/Main.java
@@ -1,7 +1,23 @@
 package hust.soict.dsai.javafx;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
+
+
     public static void main (String[] args) {
-        return;
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Painter.fxml"));
+        stage.setTitle("Painter");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
